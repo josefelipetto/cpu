f56e0456261ce108a7c0357f3963a4f80c19ed17
José Henrique Felipetto <jose.felipetto@pucpr.br>
2018-10-08 12:38:33 -0300

    RAM include

---
+++ cpu.cpp
@@ -0,0 +1,20 @@
+//
+// Created by josefelipetto on 08/10/18.
+//
+
+// RAM
+typedef int word;
+
+typedef struct {
+    unsigned int addr;
+    word content;
+} memoryCell ;
+
+memoryCell RAM[15999];
+
+
+
+
+
+
+
